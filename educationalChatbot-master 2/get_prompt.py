2b2ec90e9d83b01c3b6013a8f6b09343dd6d6306
Gianmarco Noce <64771575+gian0608@users.noreply.github.com>
2023-12-29 18:34:14 +0100

    Add files via upload

---
+++ educationalChatbot-master 2/get_prompt.py
@@ -0,0 +1,88 @@
+import streamlit
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
+from langchain.schema import SystemMessage
+
+
+def load_prompt(content):
+    template = template = (""""I want you to act as a university software engineering professor delivering engaging 
+    and concise lectures to Italian students. Your expertise lies in explaining SWEBOK chapters in English to your 
+    students in Italian. You are an expert educator and are responsible for guiding the user through this lesson plan. 
+    Ensure you help them progress appropriately and encourage them along the way. If they ask off-topic questions, 
+    politely decline and remind them to stay on topic. Please limit responses to one concept or step at a time. Each 
+    step should contain no more than ~5 lines. Ensure they fully understand before proceeding. This is an interactive 
+    lesson - engage and guide them, don't lecture. ----------------- {content} ----------------- End of Content.
+
+    End of Lesson."""
+                           .format(content=content))
+
+    prompt_template = ChatPromptTemplate(messages=[
+        SystemMessage(content=template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{input}")
+    ])
+    return prompt_template
+
+
+def load_prompt_with_questions(content):
+    template = """"I want you to act as a university software engineering professor delivering engaging and concise 
+    lectures to Italian students. Your expertise lies in explaining SWEBOK chapters in English to your students in 
+    Italian.You are an expert educator, and are responsible for walking the user through this lesson plan. You should 
+    make sure to guide them along, encouraging them to progress when appropriate. If they ask questions not related 
+    to this getting started guide, you should politely decline to answer and remind them to stay on topic. You should 
+    ask them questions about the instructions after each instructions and verify their response is correct before 
+    proceeding to make sure they understand the lesson. Whenever the user answers correctly to your questions, 
+    write these exact words: -Hai risposto correttamente. If they make a mistake, give them good explanations and 
+    encourage them to answer your questions, instead of just moving forward to the next step.
+
+    Please limit any responses to only one concept or step at a time.
+    Each step show only be ~10 lines at MOST.
+    Make sure they fully understand that before moving on to the next. 
+    This is an interactive lesson - do not lecture them, but rather engage and guide them along!
+    -----------------
+
+    {content}
+
+    -----------------
+    End of Content.
+
+    Only one question per message! Only one lesson per message!
+    ...
+
+    """.format(content=content)
+
+    prompt_template = ChatPromptTemplate(messages=[
+        SystemMessage(content=template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{input}")
+    ])
+    return prompt_template
+
+
+def get_lesson_guide(connection):
+    cursor = connection.cursor()
+
+    lesson_guides = {}
+    query = ("SELECT l.id, l.nome, l.descrizione, l.percorso_file "
+             "FROM Lezioni l JOIN Utenti_Lezioni ul ON l.id = ul.id_lezione "
+             "WHERE ul.username_utente = %s")
+    values = (streamlit.session_state.username,)
+
+    try:
+        cursor.execute(query, values)
+
+        # Estrai i risultati
+        results = cursor.fetchall()
+
+        # Itera attraverso i risultati e aggiungi le informazioni a lesson_guides
+        for result in results:
+            id_lezione, nome_lezione, descrizione, percorso_file = result
+            lesson_guides[nome_lezione] = {
+                "id": id_lezione,
+                "description": descrizione,
+                "file": percorso_file
+
+            }
+    except Exception as e:
+        print(f"Errore durante l'esecuzione della query: {e}")
+
+    return lesson_guides
---
+++ educationalChatbot-master 2/main.py
@@ -0,0 +1,115 @@
+from langchain_core.messages import AIMessage
+import Authentication as aut
+import Lezioni as lz
+import streamlit as st
+
+connection = aut.connetti_database()
+
+
+def autenticazione(connection):
+    st.session_state.username = None
+    lz.reset_lesson()
+    st.title('Autenticazione')
+
+    st.sidebar.image("logo-night.svg", use_column_width=True)
+
+    choice = st.sidebar.selectbox("Scegli", ["Login", "Registrati"])
+
+    if choice == "Login":
+
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        if st.button("Login"):
+            if aut.verifica_credenziali(username, password, connection):
+                st.success("Login effettuato")
+                st.session_state.username = username
+                st.session_state.pagina_corrente = 'Lezioni'
+            else:
+                st.error("Credenziali non valide")
+
+    elif choice == "Registrati":
+
+        email = st.text_input("Email")
+        username = st.text_input("Username")
+        api_key = st.text_input("OPENAI API-KEY", type="password")
+        password = st.text_input("Password", type="password")
+
+        if aut.validate_password(password) and aut.is_api_key_valid(api_key):
+            confirm_password = st.text_input("Conferma Password")
+
+            if st.button("Register") and aut.validated_password == confirm_password:
+                aut.aggiungi_utente_al_database(username, password, email, api_key, connection)
+                st.success("Utente registrato!")
+            elif aut.validated_password != confirm_password:
+                st.error("Le password inserite non coincidono")
+
+
+def lezioni(connection):
+    st.title('Lezioni')
+    lz.setup_page()
+
+
+    st.sidebar.image("logo-night.svg", use_column_width=True)
+    if st.sidebar.button("Logout"):
+        st.session_state.pagina_corrente = 'Autenticazione'
+
+    lesson_selection = st.sidebar.selectbox("Seleziona Lezione",
+                                            list(lz.get_prompt.get_lesson_guide(connection).keys()))
+
+    cursor = connection.cursor()
+
+    query = "SELECT API_key FROM Utenti WHERE Username = %s"
+    values = (st.session_state.username,)
+
+    cursor.execute(query, values)
+
+    # Estrai i risultati
+    api_key = cursor.fetchall()[0][0]
+
+    lesson_info = lz.get_prompt.get_lesson_guide(connection)[lesson_selection]
+    lesson_content = lz.load_lesson_content(lesson_info["file"])
+
+    lesson_type = st.sidebar.radio("Come vuoi svolgere la lezione?",
+                                   ["Lezione interattiva", "Lezione di ripasso con domande"])
+
+    if st.session_state.get("current_lesson") != lesson_selection or st.session_state.get(
+            "current_lesson_type") != lesson_type:
+        st.session_state["current_lesson"] = lesson_selection
+        st.session_state["current_lesson_type"] = lesson_type
+        st.session_state["messages"] = [AIMessage(
+            content="Benvenuto " + str(st.session_state.username) + "! Sono AiDe, il tuo assistente virtuale che ti "
+                                                                    "guiderà nell'apprendimento"
+                                                                    " dell'ingegneria del software. Scrivimi un messaggio non appena sei pronto per iniziare!"
+        )]
+
+    lz.display_lesson(lesson_selection, lesson_info)
+    lz.handle_messages()
+    if prompt := st.chat_input():
+        st.chat_message("user").write(prompt)
+        lz.run_langchain_model(prompt, lesson_type, lesson_content, lesson_selection, api_key)
+
+    lz.download_chat()
+
+    st.sidebar.button("Reimposta Lezione", on_click=lz.reset_lesson)
+    container_checkbox = st.sidebar.container()
+    container_button = st.empty()
+
+    if lesson_type == "Lezione di ripasso con domande":
+        if st.sidebar.checkbox('Mostra Progressi'):
+            container_button.empty()
+            container_centrale = lz.avanzamento_barra(connection)
+        else:
+            container_centrale = st.empty()
+
+
+###################################################################
+
+if 'pagina_corrente' not in st.session_state:
+    st.session_state.pagina_corrente = 'Autenticazione'
+
+# Mostra la pagina corrente
+if st.session_state.get('pagina_corrente') == 'Autenticazione':
+    autenticazione(connection)
+elif st.session_state.get('pagina_corrente') == 'Lezioni':
+    lezioni(connection)
